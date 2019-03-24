b62bdc01988e7b9d492a457e14fc1db14eb16f8d
tscotce <tscotce@gmail.com>
2019-03-24 16:33:41 -0400

    Added post /signup method to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,14 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if params[:first_name].present? && params[:last_name].present? && params[:username].present? && params[:email].present? && params[:password].present?
+      @user = User.create(first_name: params[:first_name], last_name: params[:last_name], username: params[:username], email: params[:email], password: params[:password])
+      session[:id] = @user.id
+      redirect to "events/index"
+    else redirect to "users/signup"
+    end
+  end
+
 
 end
