440e2f095e67b84445be29457ebc3e2ca164e746
tscotce <tscotce@gmail.com>
2019-08-13 20:53:02 -0400

    moved scraper methods to helpers and deleted scraper model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,53 @@ helpers do
     def logout!
       session.clear
     end
+
+    def get_page
+      @@doc ||= scrape
+    end
+
+    def scrape
+      Nokogiri::HTML(open("https://www.amnh.org/calendar?facetsearch=1"))
+    end
+
+    def get_events
+      self.get_page.css(".mod.event")
+    end
+
+    def make_events
+      get_events.each do |post|
+        @event = Event.new
+        # @@all << event
+        @event.type = post.css("p.category").text
+        @event.name = post.css("a").text.strip
+        @event.date = post.css("p.date").text
+        text = post.css("p").text
+        @event.description = text.to_s.gsub(/#{event.date}/,"").gsub(/#{event.type}/,"").gsub(" Members Only","").gsub(" Sold Out","").gsub(" Free With Museum Admission","").gsub("â"," ")
+        @event.website = post.css("a").first["href"]
+        @event.save
+      end
+      self.all
+    end
+
+    def sort_events
+      self.make_events
+      self.all.sort_by! {|event| event.type}
+    end
+
+    def make_types
+      self.sort_events.uniq {|event| event.type}
+    end
+
+    def print_types
+      Event.make_types.each.with_index(1) do |event, i|
+        puts "#{i}. #{event.type}"
+      end
+    end
+
+    def list_types
+      # puts "Here are types of upcoming events at the American Museum of Natural History (AMNH):"
+      @event_types = self.print_types
+      # puts "Enter the number corresponding to the type of event you'd like more information on or type 'exit':"
+    end
 	end
 end
--- app/models/scraper.rb
+++
@@ -1,9 +0,0 @@
-class Scraper < ActiveRecord::Base
-  def self.get_page
-    @@doc ||= scrape
-  end
-
-  def self.scrape
-    Nokogiri::HTML(open("https://www.amnh.org/calendar?facetsearch=1"))
-  end
-end
