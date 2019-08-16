caa73ba400f6688d64eb1f290625055643cc1cb0
Tatyana Scotce <tscotce@gmail.com>
2019-08-16 20:30:31 +0000

    added style.css file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,37 +25,37 @@ helpers do
       session.clear
     end
 
-    def get_page
-      @event = Event.new
-      @doc ||= scrape
-    end
+    # def get_page
+    #   @event = Event.new
+    #   @doc ||= scrape
+    # end
 
-    def scrape
-      # require 'open-uri'
-      html_doc = open("https://www.amnh.org/calendar?facetsearch=1")
-      nokogiri_doc = Nokogiri::HTML(html_doc)
-      # binding.pry
-    end
+    # def scrape
+    #   # require 'open-uri'
+    #   html_doc = open("https://www.amnh.org/calendar?facetsearch=1")
+    #   nokogiri_doc = Nokogiri::HTML(html_doc)
+    #   # binding.pry
+    # end
 
-    def get_events
-      get_page.css(".mod.event")
-    end
+    # def get_events
+    #   get_page.css(".mod.event")
+    # end
 
-    def make_events
-      # binding.pry
-      Event.connection
-      get_events.each do |post|
-        # @event = Event.new
-        @event.type = post.css("p.category").text
-        @event.name = post.css("a").text.strip
-        @event.date = post.css("p.date").text
-        @event.website = post.css("a").first["href"]
-        text = post.css("p").text
-        @event.description = text.to_s.gsub(/#{event.date}/,"").gsub(/#{event.type}/,"").gsub(" Members Only","").gsub(" Sold Out","").gsub(" Free With Museum Admission","").gsub("â"," ")
-        @event.save
-      end
-      Event.all
-    end
+    # def make_events
+    #   # binding.pry
+    #   Event.connection
+    #   get_events.each do |post|
+    #     # @event = Event.new
+    #     @event.type = post.css("p.category").text
+    #     @event.name = post.css("a").text.strip
+    #     @event.date = post.css("p.date").text
+    #     @event.website = post.css("a").first["href"]
+    #     text = post.css("p").text
+    #     @event.description = text.to_s.gsub(/#{event.date}/,"").gsub(/#{event.type}/,"").gsub(" Members Only","").gsub(" Sold Out","").gsub(" Free With Museum Admission","").gsub("â"," ")
+    #     @event.save
+    #   end
+    #   Event.all
+    # end
 
 	end
 end
